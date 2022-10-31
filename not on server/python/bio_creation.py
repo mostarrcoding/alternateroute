5172f0d66f342a89b732b18aac4ebc38594633e4
mkstarr <starrtennis@gmail.com>
2022-10-31 13:33:52 -0700

    issue 8 release

--- not on server/python/bio_creation.py
+++ not on server/python/bio_creation.py
@@ -2,7 +2,7 @@
 import os
 
 def make_href_list():
-    path = "../bios/"
+    path = "../../bios/"
     bio_HTML_files = os.listdir(path)
     print(bio_HTML_files)
     sorted_bio_HTML_files = sorted(bio_HTML_files, key=lambda x: x.split("_")[-1])
@@ -14,12 +14,12 @@ def make_href_list():
         bio_link = link_string.replace("{{filename}}", filename).replace("{{author}}", author)
         bio_links.write(bio_link + "\n")
 
-#make_href_list()
+make_href_list()
 
 
 
 def make_bio_HTML_files():
-    bioTemplate_file = open("../not on server/bio_template.html", "r")
+    bioTemplate_file = open("../bio_template.html", "r")
     bioTemplate_string = bioTemplate_file.read()
     import authors as auth
     authors = auth.authors()
@@ -33,12 +33,12 @@ def make_bio_HTML_files():
         bio = bio.replace("\n", "<br />")
         authorBioHTML = bioTemplate_string.replace("{{creator_name}}", name).replace("{{creator bio}}", bio).replace("â€™", "'")
         #print(authorBioHTML)
-        bio_html = open("../bios/" + name.replace(" ", "_") + ".html", "w", encoding="utf-8")
+        bio_html = open("../../bios/" + name.replace(" ", "_") + ".html", "w", encoding="utf-8")
         bio_html.write(authorBioHTML)
         bio_html.close()
 
 
-#make_bio_HTML_files()
+make_bio_HTML_files()
 
 def make_list_for_copyright_page():
     path = "/media/mstarr/Alice/writing/poetry/[ALTERNATE ROUTE]/Issue 8 - Octobor 31 2022/bios/"
