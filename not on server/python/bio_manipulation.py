7b4f168308fb40e7b463598da30e05cae195bc04
mostarrcoding <michaelstarr.coding@gmail.com>
2023-08-12 20:30:45 -0700

    updated bio list for issue 10 + code to do so

--- not on server/python/bio_manipulation.py
+++ not on server/python/bio_manipulation.py
@@ -3,6 +3,27 @@ Standard file naming convention for bio files is "[first name] [last name] . txt
 '''
 
 import os
+import shutil
+
+'''
+bio.txt for each author located in author submission folder "submission_dir"
+get author name
+for each biography
+rename bio.txt to author name . txt and
+move to addr
+'''
+def move_rename_bios(submissions_dir): #submissions_dir e.g. "C:\Users\starr\[Alternate Route]\Issue 10 - Summer 2023\Submissions\Placed"
+    new_addr = input("Where would you like to move all biographies?") #e.g. "C:\\Users\\starr\\[Alternate Route]\\Issue 10 - Summer 2023\\bios\"
+    for filename in os.listdir(submissions_dir):
+        f = os.path.join(submissions_dir, filename)
+        print(f)
+        author_name = f.split('\\')[-1].replace(' ', '_')
+        print(author_name)
+        new_biography_filename = author_name + '.txt.'
+        try:
+            shutil.copy(f + '\\bio.txt', new_addr + '\\' + new_biography_filename)
+        except:
+            continue
 
 def get_addr():
     '''Returns the absolute path as input by the user upon query for directory containing author biography files.'''
@@ -30,16 +51,13 @@ def make_bio_HTML_files(addr, authors):
     '''Creates HTML files, one for each author biography, containing their biography in a <p> under their name in <h2>.'''
     bioTemplate_file = open("../bio_template.html", "r")
     bioTemplate_string = bioTemplate_file.read()
-    #print(authors)
     for author in authors:
-        #print(author["name"])
-        #print(author["bio"])
         name = author
+        html_name = name.replace("_", " ")
         bio_file = open(addr + name + ".txt", "r")
         bio = bio_file.read()
         bio = bio.replace("\n", "<br />")
-        authorBioHTML = bioTemplate_string.replace("{{creator_name}}", name).replace("{{creator bio}}", bio).replace("â€™", "'")
-        #print(authorBioHTML)
+        authorBioHTML = bioTemplate_string.replace("{{creator_name}}", name.replace('_', ' ')).replace("{{creator bio}}", bio).replace("â€™", "'")
         bio_html = open("../../bios/" + name.replace(" ", "_") + ".html", "w", encoding="utf-8")
         bio_html.write(authorBioHTML)
         bio_html.close()
@@ -48,7 +66,6 @@ def make_href_list():
     '''Creates list of <a> links for input into bio_list.html. Does the whole list, not just new authors. No fancy new file detection.'''
     path = "../../bios/"
     bio_HTML_files = os.listdir(path)
-    #print(bio_HTML_files)
     sorted_bio_HTML_files = sorted(bio_HTML_files, key=lambda x: x.split("_")[-1])
     link_string = "<a class=\"bio\" href=\"bios/{{filename}}\">{{author}}</a>"
     bio_links = open("bio_links.txt" , "w", encoding="utf-8")
@@ -57,6 +74,8 @@ def make_href_list():
         author = bio_HTML_file.replace("_", " ").replace(".html", "")
         bio_link = link_string.replace("{{filename}}", filename).replace("{{author}}", author)
         bio_links.write(bio_link + "\n")
+    #You have to manually copy and paste from bio_links.txt (in python folder) to bio_list.html. Overwrite current bio list with new list
+    #(it contains all authors/artists, not just new ones).
 
 def make_list_for_copyright_page(path):
     '''Prints, so you can copy-paste, a last-name-alphabetized comma+space-delimited list (of type string) for the recogntions in the
